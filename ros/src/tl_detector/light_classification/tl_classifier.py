8418c08012a84ceb882396b2ff30d5f81f700709
Levin Jian <jianzhirong@gmail.com>
2017-09-26 07:14:39 +0800

    classify image stream

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -7,8 +7,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../.
 # print(sys.path)
 from nets import inception_v4
 from preprocessing import inception_preprocessing
+from datasets import trafficlight_to_tfrecords
 from nets.tl_model import TLModel
 import time
+import glob
 
 class TLClassifier(object):
     def __init__(self):
@@ -40,7 +42,7 @@ class TLClassifier(object):
         self.isess = isess
         return
 
-    def get_classification(self, image):
+    def get_classification(self, image,show_time=False):
         """Determines the color of the traffic light in the image
 
         Args:
@@ -56,24 +58,35 @@ class TLClassifier(object):
         predictons = self.isess.run(self.predictions,feed_dict={self.img_input: image})
         elapsed = time.time()
         elapsed = elapsed - start
-        print('inference : {:.3f} seconds.'.format(elapsed))
+        if show_time:
+            print('inference : {:.3f} seconds.'.format(elapsed))
         
         return predictons[0]
     
     def run(self):
         traffic_clasiffication_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../traffic_light_classifier/data'))
-        img_paths = ['sim_images/GREEN/0000120_147_784.jpg',
-                         'sim_images/GREEN/0000119_148_784.jpg',
-                         'sim_images/GREEN/0000119_148_784.jpg',
-                         'sim_images/RED/0000052_40_318.jpg',
-                         'sim_images/YELLOW/0000164_103_784.jpg']
-        img_paths = [traffic_clasiffication_path + '/'+ img_path for img_path in img_paths]
+        dataset_dir = traffic_clasiffication_path + '/sim_images/GREEN'
+        img_paths = glob.glob(dataset_dir + '/**/*.jpg', recursive=True)
+#         img_paths = ['sim_images/GREEN/0000120_147_784.jpg',
+#                          'sim_images/GREEN/0000119_148_784.jpg',
+#                          'sim_images/GREEN/0000119_148_784.jpg',
+#                          'sim_images/RED/0000052_40_318.jpg',
+#                          'sim_images/YELLOW/0000164_103_784.jpg']
+#         img_paths = [traffic_clasiffication_path + '/'+ img_path for img_path in img_paths]
+        correct_pred = 0
         for img_path in img_paths:
             img = cv2.imread(img_path, cv2.IMREAD_COLOR)
             
-            res = self.get_classification(img)
+            pred_label = self.get_classification(img) 
+            gt_label = trafficlight_to_tfrecords.class_names_to_ids[img_paths[0].split('/')[-2]]
             
-            print("traffic light={}".format(res))
+            if pred_label == gt_label:
+                correct_pred += 1.0
+            else:
+                print("actual={},pred={},image={}".format(gt_label, pred_label,img_path))
+        print("acc={}".format(correct_pred/len(img_paths)))
+            
+            
         return
 
 
